b3a6b93e767b3c374159ccf9c74c87896913acde
Alexandre Senges <alexandre@senges.ch>
2019-10-19 18:23:05 +0200

    add classes Business and Market

---
+++ src/agents/Token.py
@@ -0,0 +1,3 @@
+class Token:
+    def __init__(self, owner):
+        self.owner = owner
--- src/agents/Villager.py
+++ src/agents/Villager.py
@@ -2,4 +2,17 @@ class Villager:
     def __init__(self):
         self.bank = 0
         self.consumption = 0
-        self.tokens = 0
+        self.tokens = set()
+
+    def credit(self, price):
+        self.bank += price
+
+    def debit(self, price):
+        self.bank -= price
+
+    def add_token(self, token):
+        self.tokens.add(token)
+
+    def remove_token(self, token):
+        self.tokens.remove(token)
+
