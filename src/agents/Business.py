f54f844c6d84cf04ec3f054eb85e4b6a68ab8d55
Alexandre Senges <alexandre@senges.ch>
2019-10-19 21:00:34 +0200

    fix consumption and add random variables

--- src/agents/Business.py
+++ src/agents/Business.py
@@ -3,21 +3,21 @@ from .Villager import Villager
 CONS_OFFSET = 0.1
 MAX_CONS = 2
 
-class Business(Villager):
-    def __init__(self):
-        super().__init__()
 
-    def consumption(self):
-        pass
+class Business(Villager):
+    def __init__(self, fixed_consumption, variable_consumption):
+        super().__init__(0, 0)
+        self.fixed_consumption = fixed_consumption
+        self.variable_consumption = variable_consumption
 
     def needed_consumption_update(self, time):
         t = time % 24
 
         if t < 7 or t > 19:
-            self.needed_consumption = CONS_OFFSET
+            self.needed_consumption = self.fixed_consumption
 
         elif t == 8 or t == 18:
-            self.needed_consumption = (CONS_OFFSET+MAX_CONS)/2
+            self.needed_consumption = (self.fixed_consumption+self.variable_consumption)/2
 
         else:
-            self.needed_consumption = CONS_OFFSET
+            self.needed_consumption = self.variable_consumption
