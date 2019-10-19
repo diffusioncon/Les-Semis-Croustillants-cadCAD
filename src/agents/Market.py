7277c6990793658a2c4e64191dcc4fae50c9b4dc
Alexandre Senges <alexandre@senges.ch>
2019-10-19 19:05:06 +0200

    create market and bid-ask logic

--- src/agents/Market.py
+++ src/agents/Market.py
@@ -6,15 +6,32 @@ class Market:
     _ask = []
     last_price = 0
 
-    def ask(self, token, price):
-        heappush(self._ask, (price, token))
+    def ask(self, token):
+        token.put_on_market()
+        if len(self._bid) and self._bid[-1][0] >= self.last_price * .98:
+            price, quantity, buyer = self._bid[-1]
+            self.transaction(token, buyer, price)
+            if quantity == 1:
+                heappop(self._bid)
+            else:
+                self._bid[-1] = (price, quantity-1, buyer)
+            return True
+        heappush(self._ask, (self.last_price*.98, token))
+        return False
 
-    def bid(self, villager, price, quantity):
-        while len(self._ask) and self._ask[0][0] <= price:
+    def bid(self, villager, quantity):
+        bid_price = self.last_price * 1.02
+        while len(self._ask) and self._ask[0][0] <= bid_price:
             price, token = heappop(self._ask)
-            villager.add_token(token)
-            villager.debit(price)
-            owner = token.owner
-            owner.credit(price)
-            owner.remove_token(token)
-        heappush(self._bid, (price, quantity))
+            self.transaction(token, villager, price)
+        heappush(self._bid, (bid_price, quantity, villager))
+
+    def transaction(self, token, buyer, price):
+        owner = token.owner
+        owner.credit(price)
+        owner.remove_token(token)
+        buyer.add_token(token)
+        buyer.debit(price)
+        token.remove_from_market()
+        self.last_price = price
+
--- src/agents/Token.py
+++ src/agents/Token.py
@@ -1,3 +1,10 @@
 class Token:
     def __init__(self, owner):
         self.owner = owner
+        self.on_market = False
+
+    def put_on_market(self):
+        self.on_market = True
+
+    def remove_from_market(self):
+        self.on_market = False
