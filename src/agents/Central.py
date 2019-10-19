c8e9b94e6497ce6731c1832985d9899baa0296a3
Alexandre Senges <alexandre@senges.ch>
2019-10-19 21:22:42 +0200

    trade consume

--- src/agents/Central.py
+++ src/agents/Central.py
@@ -3,6 +3,7 @@ from .Panel import Panel
 from .Storage import Storage
 from .Villager import Villager
 from .Business import Business
+from .Token import Token
 from numpy import pi, exp
 
 
@@ -18,21 +19,36 @@ class Central:
         self.step_production = 0
         self.m_step_production = 0
         self.time = 7
+        self.current_token_id = 0
         self.init_simulation()
 
     def init_simulation(self):
         for i in range(self.nb_villagers):
             self.add_villager()
-        for i in range(self.nb_villagers/100):
+        for i in range(self.nb_villagers / 100):
             self.add_business()
 
     def step(self):
         self.step_production = 0
         self.m_step_production = 0
         self.produce()
+        self.hospital_consumption()
+        self.distribute_tokens()
+        self.compute_needed_consumption()
+        self.trade()
         self.consume()
         self.store()
         self.time += 1
+
+    def distribute_tokens(self):
+        quantity = self.step_production / self.token_per_kW
+        for _ in range(quantity):
+            villager = self.villagers[self.current_token_id]
+            villager.add_token(Token())
+            if self.current_token_id == self.nb_villagers - 1:
+                self.current_token_id = 0
+            else:
+                self.current_token_id += 1
 
     def add_panel(self):
         self.panels.append(Panel())
@@ -52,26 +68,23 @@ class Central:
             self.step_production += panel.produce(self.time)
         self.m_step_production = self.step_production
 
-    def consume(self):
-        # Compute consumption
-
-        # Update hospital consumption
-        self.hospital_consumption()
-        # Update villagers needed consumption
+    def compute_needed_consumption(self):
         for v in self.villagers:
             v.needed_consumption_update(self.time)
-        # Update businesses needed consumption
         for b in self.businesses:
             b.needed_consumption_update(self.time)
 
+    def trade(self):
+        pass
+
+    def consume(self):
+        pass
 
     def store(self):
         for storage in self.storages:
-            if self.step_production:
-                self.step_production = storage.fill(self.step_production)
-            else:
-                break
-        self.distribute_tokens((self.m_step_production - self.step_production) / self.token_per_kW)
+            if not self.step_production:
+                return
+            self.step_production = storage.fill(self.step_production)
 
     def take_from_storage(self, quantity):
         for storage in self.storages:
@@ -80,17 +93,13 @@ class Central:
                 return quantity
         return quantity
 
-    def distribute_tokens(self, quantity):
-        # Give N tokens to each villager
-        pass
-
     def hospital_consumption(self):
         t = self.time % 24
         offset = 2
         scale = 3
         mu = 13
-        var = 4**2
-        hospital_consumption = offset + scale*1/(2*pi*var)*exp(-(t-mu)**2/2/var)
+        var = 4 ** 2
+        hospital_consumption = offset + scale * 1 / (2 * pi * var) * exp(-(t - mu) ** 2 / 2 / var)
         delta = self.step_production - hospital_consumption
         if delta > 0:
             self.step_production -= hospital_consumption
